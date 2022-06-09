30dddd2cf2455ddf0f2d3570f08f610d9d94cf29
Alexaking11122 <106728456+Alexaking11122@users.noreply.github.com>
2022-06-09 17:19:23 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['+923189718115']
+global.premium = ['+923189718115']
+global.ownernomer = 'Rayan alexa hacker'
+global.ownername = '🐦R M lovers'
+global.botname = 'RAYAN-MADO'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'RAYAN-MADO'
+global.author = '🐦R M Lovers'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
